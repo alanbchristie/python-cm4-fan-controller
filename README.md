30f2524974247573c60d3894d14ce47502500554
Alan Christie <alan.christie@matildapeak.com>
2023-12-21 22:15:30 +0000

    ci: Use of pre-commit

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 # Python CM4 Fan Controller
 A temperature-based fan controller for the Compute Module 4 IO board.
 
+## Contributing
+The project uses: -
+
+- [pre-commit] to enforce linting of files prior to committing them to the upstream repository
+- [Commitizen] to enforce a [Conventional Commit] commit message format
+- [Black] and [isort] as a code formatter
+
+You MUST comply with these choices in order to contribute to the project.
+
+To get started review the pre-commit utility and the conventional commit style
+and then set-up your local clone by following the Installation and Quick Start sections: -
+
+    poetry shell
+    poetry install
+    pre-commit install -t commit-msg -t pre-commit
+
+Now the project's rules will run on every commit, and you can check the current health
+of your clone with: -
+
+    pre-commit run --all-files
+
 ---
+
+[black]: https://black.readthedocs.io/en/stable/
+[commitizen]: https://commitizen-tools.github.io/commitizen/
+[conventional commit]: https://www.conventionalcommits.org/en/v1.0.0/
+[isort]: https://pycqa.github.io/isort/
+[pre-commit]: https://pre-commit.com
--- controller.py
+++ controller.py
@@ -10,68 +10,72 @@ My fan is a Waveshare, part number: CM4-FAN-3007-12V and the
 practical Fan Speed Range (found empirically) is between between 45 (18%) and 255 (100%).
 The fastest speed while remaining quiet is about 70 (27%).
 """
+import logging
 import os
+import sys
 import time
 from typing import Tuple
 
-import smbus  # type: ignore
+import smbus  # pylint: disable=import-error
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logger: logging.Logger = logging.getLogger()
 
 # Hysteresis: 2 degrees?
 # So if the trigger for point 4 is 70 degrees
 # the fan goes to 100% at 70 degrees or greater
 # and remains at 100% until the temperature
 # drops to 68 degrees (or less)
-HYSTERESIS: int = int(os.environ.get('HYSTERESIS', '2'))
+HYSTERESIS: int = int(os.environ.get("HYSTERESIS", "2"))
 
-TEMPERATURE_POINT_1: int = int(os.environ.get('TEMPERATURE_POINT_1', '35'))
-TEMPERATURE_POINT_2: int = int(os.environ.get('TEMPERATURE_POINT_1', '60'))
-TEMPERATURE_POINT_3: int = int(os.environ.get('TEMPERATURE_POINT_1', '65'))
-TEMPERATURE_POINT_4: int = int(os.environ.get('TEMPERATURE_POINT_1', '70'))
+TEMPERATURE_POINT_1: int = int(os.environ.get("TEMPERATURE_POINT_1", "35"))
+TEMPERATURE_POINT_2: int = int(os.environ.get("TEMPERATURE_POINT_1", "60"))
+TEMPERATURE_POINT_3: int = int(os.environ.get("TEMPERATURE_POINT_1", "65"))
+TEMPERATURE_POINT_4: int = int(os.environ.get("TEMPERATURE_POINT_1", "70"))
 
 # A tuple of band speeds (as a percent of full speed).
 BAND_SPEED: Tuple[int, int, int, int, int] = (
-    int(os.environ.get('BAND_0_SPEED', '0')),
-    int(os.environ.get('BAND_1_SPEED', '25')),
-    int(os.environ.get('BAND_2_SPEED', '50')),
-    int(os.environ.get('BAND_3_SPEED', '75')),
-    int(os.environ.get('BAND_4_SPEED', '100')),
+    int(os.environ.get("BAND_0_SPEED", "0")),
+    int(os.environ.get("BAND_1_SPEED", "25")),
+    int(os.environ.get("BAND_2_SPEED", "50")),
+    int(os.environ.get("BAND_3_SPEED", "75")),
+    int(os.environ.get("BAND_4_SPEED", "100")),
 )
 
-MEASUREMENT_INTERVAL_S: int = int(os.environ.get('MEASUREMENT_INTERVAL_S', '8'))
+MEASUREMENT_INTERVAL_S: int = int(os.environ.get("MEASUREMENT_INTERVAL_S", "8"))
 
 # Hardware details of the fan controller
 # Bus ID of 10 implies the device /dev/i2c-10 (port I2C10)
 BUS_ID: int = 10
-FAN_ADDR = 0x2f
+FAN_ADDR = 0x2F
 FAN_CONTROL_COMMAND = 0x30
 
-# The CPU temperature is written to a sys file.
+# The CPU temperature is written to a sys file.
 # It's value is milli-degrees Celsius.
-CPU_TEMPERATURE_FILE = '/sys/class/thermal/thermal_zone0/temp'
+CPU_TEMPERATURE_FILE = "/sys/class/thermal/thermal_zone0/temp"
 
 HIGHEST_BAND: int = len(BAND_SPEED) - 1
 CURRENT_BAND: int = 0
 CURRENT_TEMPERATURE: float = 0.0
 
 # Create the I2C bus object
-SMBUS =  smbus.SMBus(BUS_ID)
+SMBUS = smbus.SMBus(BUS_ID)
 
 
-def set_fan_speed(temperature_band: int) -> None:
-    """Sets the fan speed, given a percentage.
-    """
-    global CURRENT_BAND
-    global BAND_SPEED
-    assert temperature_band >= 0 and temperature_band <= HIGHEST_BAND
+def set_fan_speed(band: int) -> None:
+    """Sets the fan speed, given a percentage."""
+    global CURRENT_BAND  # pylint: disable=global-statement
+    assert 0 <= band <= HIGHEST_BAND
 
-    if temperature_band == CURRENT_BAND:
+    if band == CURRENT_BAND:
         return
 
-    speed_percent: int = BAND_SPEED[temperature_band]
+    speed_percent: int = BAND_SPEED[band]
+    logging.info("Setting fan speed to %s%%", speed_percent)
+
     speed_int = int(speed_percent * 255 / 100)
-    print(f'Setting fan speed to {speed_percent}%')
     SMBUS.write_byte_data(FAN_ADDR, FAN_CONTROL_COMMAND, speed_int)
-    CURRENT_BAND = temperature_band
+    CURRENT_BAND = band
 
 
 def get_cpu_temperature() -> float | None:
@@ -79,82 +83,82 @@ def get_cpu_temperature() -> float | None:
     If it fails, or we read nothing we return None. The caller
     should assume the temperature is critical under this condition.
     """
-    global CURRENT_TEMPERATURE
+    global CURRENT_TEMPERATURE  # pylint: disable=global-statement
 
     if os.path.isfile(CPU_TEMPERATURE_FILE):
-        with open(CPU_TEMPERATURE_FILE, 'r', encoding='utf-8') as ct_file:
+        with open(CPU_TEMPERATURE_FILE, "r", encoding="utf-8") as ct_file:
             milli_c = ct_file.readline().strip()
         temperature: float = int(milli_c) / 1000.0
         if temperature != CURRENT_TEMPERATURE:
             CURRENT_TEMPERATURE = temperature
-            print(f'Temperature is now {temperature}')
+            logging.info("Temperature is now %s", temperature)
         return temperature
     return None
 
 
-def calculate_temperature_band(temperature: float) -> int:
+def calculate_temperature_band(  # pylint: disable=too-many-branches
+    temperature: float,
+) -> int:
     """Calculate the new temperature band, given the temperature.
     The new band will depend on the current band,
     the new temperature and hysteresis.
     """
-    global CURRENT_BAND
-
-    NEW_BAND: int = CURRENT_BAND
+    new_band: int = CURRENT_BAND
     if CURRENT_BAND == 0:
         # Fan's not running - should it run?
         if temperature >= TEMPERATURE_POINT_1:
-            NEW_BAND = 1
+            new_band = 1
         elif temperature >= TEMPERATURE_POINT_2:
-            NEW_BAND = 2
+            new_band = 2
         elif temperature >= TEMPERATURE_POINT_3:
-            NEW_BAND = 3
+            new_band = 3
         elif temperature >= TEMPERATURE_POINT_4:
-            NEW_BAND = 4
+            new_band = 4
 
     elif CURRENT_BAND == 1:
         # Do we need to move up a band?
         if temperature >= TEMPERATURE_POINT_2:
-            NEW_BAND = 2
+            new_band = 2
         elif temperature >= TEMPERATURE_POINT_3:
-            NEW_BAND = 3
+            new_band = 3
         elif temperature >= TEMPERATURE_POINT_4:
-            NEW_BAND = 4
+            new_band = 4
         # Do we need to move to a lower band (there is only one below us)?
         if temperature <= TEMPERATURE_POINT_1 - HYSTERESIS:
-            NEW_BAND = 0
+            new_band = 0
 
     elif CURRENT_BAND == 2:
         # Do we need to move up a band?
         if temperature >= TEMPERATURE_POINT_3:
-            NEW_BAND = 3
+            new_band = 3
         elif temperature >= TEMPERATURE_POINT_4:
-            NEW_BAND = 4
+            new_band = 4
         # Do we need to move to a lower band?
         if temperature <= TEMPERATURE_POINT_2 - HYSTERESIS:
-            NEW_BAND = 1
+            new_band = 1
 
     elif CURRENT_BAND == 3:
         # Do we need to move up a band?
         if temperature >= TEMPERATURE_POINT_4:
-            NEW_BAND = 4
+            new_band = 4
             return 100
         # Do we need to move to a lower band?
         if temperature <= TEMPERATURE_POINT_3 - HYSTERESIS:
-            NEW_BAND = 2
+            new_band = 2
 
     elif temperature <= TEMPERATURE_POINT_4 - HYSTERESIS:
-        NEW_BAND = 3
+        new_band = 3
 
-    assert NEW_BAND >= 0 and NEW_BAND <= HIGHEST_BAND
-    if NEW_BAND != CURRENT_BAND:
-        msg: str = f'Temperature {temperature} - '
-        if NEW_BAND > CURRENT_BAND:
-            msg += f'moving up from band {CURRENT_BAND} to {NEW_BAND}'
+    assert 0 <= new_band <= HIGHEST_BAND
+    if new_band != CURRENT_BAND:
+        msg: str = f"Temperature {temperature} - "
+        if new_band > CURRENT_BAND:
+            msg += f"moving up from band {CURRENT_BAND} to {new_band}"
         else:
-            msg += f'moving down from band {CURRENT_BAND} to {NEW_BAND}'
-        print(msg)
+            msg += f"moving down from band {CURRENT_BAND} to {new_band}"
+        logging.info(msg)
 
-    return NEW_BAND
+    return new_band
 
 
 # At startup always start the fan for the highest band
@@ -163,15 +167,17 @@ set_fan_speed(HIGHEST_BAND)
 # Now periodically monitor the temperature
 # setting the fan speed using the calculated temperature band.
 while True:
-
     time.sleep(MEASUREMENT_INTERVAL_S)
 
     current_temperature: float | None = get_cpu_temperature()
     if current_temperature is None:
-        print('Temperature read failed - setting fan speed to 100%')
+        logging.info(
+            "Temperature read failed - setting fan speed to %s%%",
+            BAND_SPEED[HIGHEST_BAND],
+        )
         set_fan_speed(HIGHEST_BAND)
         continue
 
     temperature_band: int = calculate_temperature_band(current_temperature)
-    assert temperature_band >= 0 and temperature_band <= HIGHEST_BAND
+    assert 0 <= temperature_band <= HIGHEST_BAND
     set_fan_speed(temperature_band)
